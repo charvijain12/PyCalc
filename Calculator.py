470d0f833e3b267e0db10dccfa62628cc981fc21
Charvi Jain <97164074+charvijain12@users.noreply.github.com>
2023-02-06 21:41:43 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,41 @@
+def calculator():
+  while True:
+    print("---------CALCULATOR----------")
+    print("Select operation:")
+    print("Enter 'add' to add two numbers")
+    print("Enter 'subtract' to subtract two numbers")
+    print("Enter 'multiply' to multiply two numbers")
+    print("Enter 'divide' to divide two numbers")
+    print("Enter 'quit' to end the program")
+    user_input = input(": ")
+
+    if user_input == "quit":
+      break
+    elif user_input == "add":
+      num1 = float(input("Enter first number: "))
+      num2 = float(input("Enter second number: "))
+      result = num1 + num2
+      print("The answer is " + str(result))
+    elif user_input == "subtract":
+      num1 = float(input("Enter first number: "))
+      num2 = float(input("Enter second number: "))
+      result = num1 - num2
+      print("The answer is " + str(result))
+    elif user_input == "multiply":
+      num1 = float(input("Enter first number: "))
+      num2 = float(input("Enter second number: "))
+      result = num1 * num2
+      print("The answer is " + str(result))
+    elif user_input == "divide":
+      num1 = float(input("Enter first number: "))
+      num2 = float(input("Enter second number: "))
+      if num2 == 0:
+        print("Cannot divide by 0!")
+      else:
+        result = num1 / num2
+        print("The answer is " + str(result))
+    else:
+      print("Unknown input")
+
+calculator()
+
